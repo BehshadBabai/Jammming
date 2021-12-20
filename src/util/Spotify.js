cbdf77daa6359157cc83723981183f6c3c81ad5f
BehshadBabai <behshad.babai@gmail.com>
2021-12-20 01:25:18 -0800

    going to 2 commits ago

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -23,27 +23,39 @@ const Spotify = {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl;
         }
-    }
+    },
     search(term) {
         const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
-                Authorization: `Bearer ${accessToken}`;
+                Authorization: `Bearer ${accessToken}`
             }
         }).then(response => {
             return response.json();
-            ).then(jsonResponse => {
+        }).then(jsonResponse => {
                 if (!jsonResponse) {
                     return [];
                 }
                 return jsonResponse.tracks.items.map(track => ({
                     id: track.id,
                     name: track.name,
-                    artist = track.artist[0].name,
+                    artist: track.artist[0].name,
                     album: track.album.name,
                     uri: track.uri
                 }));
             });
+    },
+    savePlaylist(name, trackList) {
+        if (name || trackList.length) {
+            return;
+        }
+        const accessToken = Spotify.getAccessToken();
+        const headers = { authorization: `Bearer ${accessToken}` };
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', { headers: headers }).then(response => response.json()).then(jsonResponse => {
+            userId = jsonResponse.id;
+        });
     }
 };
 
