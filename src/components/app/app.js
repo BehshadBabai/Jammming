fd435048e867ff3dadd848fa54e57189bde794cb
BehshadBabai <behshad.babai@gmail.com>
2022-02-21 13:18:06 -0800

    fixing the whitespace in the informations paragraph

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -67,7 +67,7 @@ class App extends React.Component {
                   >
                     Email
                   </a>{' '}
-                  or
+                  or{' '}
                   <a
                     href='https://linkedin.com/in/behshadbabai'
                     target='_blank'
